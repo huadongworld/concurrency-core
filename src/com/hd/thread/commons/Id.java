c91f8c9b6ddd70d7437d33f408d0ed8e541a2744
huadong <hd2260332898@gmail.com>
2019-11-03 10:21:59 +0800

    线程Id

---
+++ src/com/hd/thread/commons/Id.java
@@ -0,0 +1,16 @@
+package com.hd.thread.commons;
+
+/**
+ * ID从1开始，JVM运行起来后，我们自己创建的线程的ID早已不是2.
+ *
+ * @author HuaDong
+ * @date 2019/11/2 21:34
+ */
+public class Id {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+        System.out.println("主线程的ID —— " + Thread.currentThread().getId());
+        System.out.println("子线程的ID —— " + thread.getId());
+    }
+}
