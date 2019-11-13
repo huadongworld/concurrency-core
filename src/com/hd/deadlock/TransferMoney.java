3eda43351ea05ad2bbbd98055dca18bc4dce2045
huadong <hd2260332898@gmail.com>
2019-11-13 22:15:14 +0800

    死锁-银行转账问题

--- src/com/hd/deadlock/TransferMoney.java
+++ src/com/hd/deadlock/TransferMoney.java
@@ -8,16 +8,16 @@ package com.hd.deadlock;
  */
 public class TransferMoney implements Runnable {
 
-    int flag = 1;
-    static Account a = new Account(500);
-    static Account b = new Account(500);
-    static Object lock = new Object();
+    private int flag = 1;
+    private static Account a = new Account(500);
+    private static Account b = new Account(500);
 
     public static void main(String[] args) throws InterruptedException {
         TransferMoney r1 = new TransferMoney();
         TransferMoney r2 = new TransferMoney();
         r1.flag = 1;
         r2.flag = 0;
+        //定义两个线程，flag = 1和0分别模拟a和b
         Thread t1 = new Thread(r1);
         Thread t2 = new Thread(r2);
         t1.start();
@@ -31,17 +31,31 @@ public class TransferMoney implements Runnable {
     @Override
     public void run() {
         if (flag == 1) {
+            //a转账给b
             transferMoney(a, b, 200);
         }
         if (flag == 0) {
+            //b转账给a
             transferMoney(b, a, 200);
         }
     }
 
+    /**
+     * 转账方法
+     *
+     * @param from   转出人
+     * @param to     收款人
+     * @param amount 转账金额
+     */
     public static void transferMoney(Account from, Account to, int amount) {
-        class Helper {
-
-            public void transfer() {
+        synchronized (from) {
+            try {
+                //模拟业务耗时
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (to) {
                 if (from.balance - amount < 0) {
                     System.out.println("余额不足，转账失败。");
                     return;
@@ -51,38 +65,16 @@ public class TransferMoney implements Runnable {
                 System.out.println("成功转账" + amount + "元");
             }
         }
-        int fromHash = System.identityHashCode(from);
-        int toHash = System.identityHashCode(to);
-        if (fromHash < toHash) {
-            synchronized (from) {
-                synchronized (to) {
-                    new Helper().transfer();
-                }
-            }
-        }
-        else if (fromHash > toHash) {
-            synchronized (to) {
-                synchronized (from) {
-                    new Helper().transfer();
-                }
-            }
-        }else  {
-            synchronized (lock) {
-                synchronized (to) {
-                    synchronized (from) {
-                        new Helper().transfer();
-                    }
-                }
-            }
-        }
     }
 
+    /**
+     * 收款账户
+     */
     static class Account {
 
         public Account(int balance) {
             this.balance = balance;
         }
-
         int balance;
     }
 }
